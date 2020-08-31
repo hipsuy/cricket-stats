d7117e2b23756bbf8fcff772c0c3e7cfd361b513
hipsuy <piyushsharmanov97@gmail.com>
2020-08-31 13:24:28 +0530

    Update cric.py to show Kohli's avg & sr over the years

--- cric.py
+++ cric.py
@@ -3,8 +3,13 @@ import sqlite3
 con = sqlite3.connect('cric-data.db')
 cur = con.cursor()
 
-
-
-
+print('\nKohli''s average and strike rate over the years:-\n')
+year = 2016
+i = 0
+for row in cur.execute('SELECT `Batting Average`, `Batting Strike Rate` FROM ipl WHERE Player = "V Kohli"'):
+    print(year+ i)
+    print('avg:',row[0],' ','sr:',row[1],'\n')
+    i = i + 1
+print('\n')
 con.commit()
 con.close()
