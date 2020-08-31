19325a132f288d451d392715752f855a3839a4da
hipsuy <piyushsharmanov97@gmail.com>
2020-08-31 13:33:50 +0530

    Added Highest Scores and Pant's stats

--- cric.py
+++ cric.py
@@ -3,13 +3,19 @@ import sqlite3
 con = sqlite3.connect('cric-data.db')
 cur = con.cursor()
 
-print('\nKohli''s average and strike rate over the years:-\n')
+print('\nKohli''s stats:-\n')
 year = 2016
-i = 0
-for row in cur.execute('SELECT `Batting Average`, `Batting Strike Rate` FROM ipl WHERE Player = "V Kohli"'):
-    print(year+ i)
-    print('avg:',row[0],' ','sr:',row[1],'\n')
-    i = i + 1
+for row in cur.execute('SELECT `Batting Average`, `Batting Strike Rate`, `Highest Score` FROM ipl WHERE Player = "V Kohli"'):
+    print(year)
+    print('avg:',row[0],' ','sr:',row[1],' ','hs:',row[2])
+    year = year + 1
+
+print('\nPant''s stats:-\n')
+year = 2016
+for row in cur.execute('SELECT `Batting Average`, `Batting Strike Rate`, `Highest Score` FROM ipl WHERE Player = "RR Pant"'):
+    print(year)
+    print('avg:',row[0],' ','sr:',row[1],' ','hs:',row[2])
+    year = year + 1
 print('\n')
 con.commit()
 con.close()
