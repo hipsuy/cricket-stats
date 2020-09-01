8393851fd14fec629d572762c27d0292cf6831e8
hipsuy <piyushsharmanov97@gmail.com>
2020-09-01 12:57:32 +0530

    Added functionality for querying batting stats of any player

--- cric.py
+++ cric.py
@@ -3,19 +3,23 @@ import sqlite3
 con = sqlite3.connect('cric-data.db')
 cur = con.cursor()
 
-print('\nKohli''s stats:-\n')
-year = 2016
-for row in cur.execute('SELECT `Batting Average`, `Batting Strike Rate`, `Highest Score` FROM ipl WHERE Player = "V Kohli"'):
-    print(year)
-    print('avg:',row[0],' ','sr:',row[1],' ','hs:',row[2])
-    year = year + 1
+print('IPL STATS')
 
-print('\nPant''s stats:-\n')
-year = 2016
-for row in cur.execute('SELECT `Batting Average`, `Batting Strike Rate`, `Highest Score` FROM ipl WHERE Player = "RR Pant"'):
-    print(year)
-    print('avg:',row[0],' ','sr:',row[1],' ','hs:',row[2])
-    year = year + 1
+while True:
+    option = int(input('\n\n1. Batting stats\n2. Bowling stats\n3. quit\n\t=>Choose option: '))
+    if option == 1:
+        name = (input('\nEnter player name: '),)
+
+        print('\nSeason\t\tMatches\t\tRuns\tAvg\t\tSR\t\tHS')
+
+        for row in cur.execute('SELECT Tournament, `Matches`, `Runds Scored`, `Batting Average`, `Batting Strike Rate`, `Highest Score` FROM ipl WHERE Player = ? ORDER BY Tournament',name):
+            print(f'{row[0]}\t{row[1]}\t\t\t{row[2]}\t\t{row[3].split(".")[0]}\t\t{(row[4]).split(".")[0]}\t\t{row[5]}')
+
+
+    elif option == 3:
+        print('BYE!')
+        break        
+
 print('\n')
 con.commit()
 con.close()
