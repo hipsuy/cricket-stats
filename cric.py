758776efac419fe8285f87cd93274f534d4ed0d7
hipsuy <piyushsharmanov97@gmail.com>
2020-09-02 14:13:22 +0530

    Now, you have to select batting stats once, and then you can keep entering player names to query batting stats

--- cric.py
+++ cric.py
@@ -8,12 +8,14 @@ print('IPL STATS')
 while True:
     option = int(input('\n\n1. Batting stats\n2. Bowling stats\n3. quit\n\t=>Choose option: '))
     if option == 1:
-        name = (input('\nEnter player name: '),)
+        while True:
+            name = (input('\nBATTING STATS\nEnter player name or enter 0 to go back to main menu: '), )
+            if name[0] == '0':
+                break
+            print('\nSeason\t\tMatches\t\tRuns\t\tAvg\t\tSR\t\tHS')
 
-        print('\nSeason\t\tMatches\t\tRuns\tAvg\t\tSR\t\tHS')
-
-        for row in cur.execute('SELECT Tournament, `Matches`, `Runds Scored`, `Batting Average`, `Batting Strike Rate`, `Highest Score` FROM ipl WHERE Player = ? ORDER BY Tournament',name):
-            print(f'{row[0]}\t{row[1]}\t\t\t{row[2]}\t\t{row[3].split(".")[0]}\t\t{(row[4]).split(".")[0]}\t\t{row[5]}')
+            for row in cur.execute('SELECT Tournament, `Matches`, `Runds Scored`, `Batting Average`, `Batting Strike Rate`, `Highest Score` FROM ipl WHERE Player = ? ORDER BY Tournament', name):
+                print(f'{row[0]}\t{row[1]}\t\t{row[2]}\t\t{row[3].split(".")[0]}\t\t{(row[4]).split(".")[0]}\t\t{row[5]}')
 
 
     elif option == 3:
